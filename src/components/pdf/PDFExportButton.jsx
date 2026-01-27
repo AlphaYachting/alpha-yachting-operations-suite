b8b5dcd48db92588a99c1ebe4aa13c2225ad5ea7
a.rittler <a.rittler@rittler.co>
2026-01-27 19:13:20 +0000

    File changes

--- src/components/pdf/PDFExportButton.jsx
+++ src/components/pdf/PDFExportButton.jsx
@@ -143,32 +143,14 @@ export default function PDFExportButton({ document: documentData, lineItems, pay
             <DialogTitle>PDF Preview</DialogTitle>
           </DialogHeader>
           {template && (
-           <div 
-             className="bg-white p-4" 
-             style={{
-               backgroundImage: template.letterhead_enabled && template.letterhead_image_url 
-                 ? `url(${template.letterhead_image_url})` 
-                 : 'none',
-               backgroundSize: 'cover',
-               backgroundRepeat: 'no-repeat',
-               backgroundPosition: 'top center',
-               backgroundAttachment: 'fixed'
-             }}
-           >
-             <div style={{
-               paddingTop: template.letterhead_enabled ? `${template.margin_top_mm || 20}mm` : '0',
-               paddingLeft: template.letterhead_enabled ? `${template.margin_left_mm || 20}mm` : '0',
-               paddingRight: template.letterhead_enabled ? `${template.margin_right_mm || 20}mm` : '0',
-               paddingBottom: template.letterhead_enabled ? `${template.margin_bottom_mm || 20}mm` : '0'
-             }}>
-               <PDFDocumentTemplate 
-                 document={documentData} 
-                 lineItems={lineItems}
-                 template={template}
-                 payments={payments}
-               />
-             </div>
-           </div>
+            <div className="bg-white p-4">
+              <PDFDocumentTemplate 
+                document={documentData} 
+                lineItems={lineItems}
+                template={template}
+                payments={payments}
+              />
+            </div>
           )}
           <div className="flex justify-end gap-3 pt-4">
             <Button variant="outline" onClick={() => setShowPreview(false)}>
