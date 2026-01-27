492e3481c4bc75eddb42ebdb151904a3bb715af8
a.rittler <a.rittler@rittler.co>
2026-01-27 20:27:13 +0000

    File changes

--- functions/generateOfferPDF.ts
+++ functions/generateOfferPDF.ts
@@ -328,3 +328,5 @@ function formatDate(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' });
 }
+
+export { generateOfferPDF };
