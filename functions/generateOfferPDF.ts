b0f95b9a5279b81789274bb8b54ea81df313a7ae
a.rittler <a.rittler@rittler.co>
2026-01-27 20:26:44 +0000

    File changes

--- functions/generateOfferPDF.ts
+++ functions/generateOfferPDF.ts
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 
-export default async function generateOfferPDF({ documentData, lineItems, templateData }) {
+async function generateOfferPDF({ documentData, lineItems, templateData }) {
   let browser;
   try {
     browser = await puppeteer.launch({ headless: 'new' });
