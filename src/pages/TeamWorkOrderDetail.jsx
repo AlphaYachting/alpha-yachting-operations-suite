2fbd1cca6579b5090934249c611a7f91a3067da9
a.rittler <a.rittler@rittler.co>
2026-01-28 18:45:05 +0000

    File changes

--- src/pages/TeamWorkOrderDetail.jsx
+++ src/pages/TeamWorkOrderDetail.jsx
@@ -322,7 +322,7 @@ export default function TeamWorkOrderDetail() {
                 }
               </CardContent>
             </Card>
-            ))
+            ))}
             </div>
           </div>
         }
