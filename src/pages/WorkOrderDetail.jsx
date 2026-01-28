18248fda95bd6e7d0947c1b10d556fbee8bb4f3c
a.rittler <a.rittler@rittler.co>
2026-01-28 19:19:31 +0000

    File changes

--- src/pages/WorkOrderDetail.jsx
+++ src/pages/WorkOrderDetail.jsx
@@ -147,6 +147,7 @@ export default function WorkOrderDetail() {
       setTechnicians(allTechs);
       setPhotos(allPhotos);
       setTeamOrder(teamOrders.length > 0 ? teamOrders[0] : null);
+      setComments(allComments);
 
       if (wo.job_id) {
         const [projectData] = await base44.entities.Job.filter({ id: wo.job_id });
