8c3364afa0b60d02e0ee7f1220aa68515cfda649
a.rittler <a.rittler@rittler.co>
2026-01-28 15:52:28 +0000

    File changes

--- src/pages/TeamMobileHome.jsx
+++ src/pages/TeamMobileHome.jsx
@@ -189,7 +189,7 @@ export default function TeamMobileHome() {
           <img 
             src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6972766f1bd9af32693610c1/27c878803_alpha-yachting-logo-weiss.png"
             alt="Alpha Yachting"
-            className="h-10 object-contain"
+            className="h-16 object-contain"
           />
           
           {/* Time & Date */}
