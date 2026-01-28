22de7bc9dc64bb2cee7de37423300329caa298f6
a.rittler <a.rittler@rittler.co>
2026-01-28 18:57:34 +0000

    File changes

--- src/pages/TeamWorkOrderDetail.jsx
+++ src/pages/TeamWorkOrderDetail.jsx
@@ -66,7 +66,6 @@ export default function TeamWorkOrderDetail() {
 
       const wo = woData[0];
       setWorkOrder(wo);
-      setIsTaskStarted(wo.status === 'In Progress' || wo.status === 'Completed');
 
       // Load related data
       const [jobData, tasksData, photosData] = await Promise.all([
