f0383b3725a0f6cad8054ba8399f7e0caefdbd00
a.rittler <a.rittler@rittler.co>
2026-01-29 13:58:58 +0000

    File changes

--- src/components/offers/OfferTaskEditor.jsx
+++ src/components/offers/OfferTaskEditor.jsx
@@ -246,7 +246,7 @@ export default function OfferTaskEditor({ tasks, setTasks }) {
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  {UNIT_OPTIONS.map(unit => (
+                  {unitOptions.map(unit => (
                     <SelectItem key={unit.value} value={unit.value}>
                       {unit.display}
                     </SelectItem>
