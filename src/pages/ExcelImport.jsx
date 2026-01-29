3a661f163d2459464eca9b469176b8b373a266c6
a.rittler <a.rittler@rittler.co>
2026-01-29 23:21:11 +0000

    File changes

--- src/pages/ExcelImport.jsx
+++ src/pages/ExcelImport.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import * as XLSX from 'npm:xlsx';
 import { 
