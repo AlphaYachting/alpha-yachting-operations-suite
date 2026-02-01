28b47a0e59be325dd234467e38c18da7cf78e7c7
a.rittler <a.rittler@rittler.co>
2026-02-01 16:44:22 +0000

    File changes

--- src/pages/WorkOrderDetail.jsx
+++ src/pages/WorkOrderDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { Link, useSearchParams } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { 
   ArrowLeft, 
   Calendar,
