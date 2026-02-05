133b85e20c0f4f70fc7a309eb17e43579ef18f4b
a.rittler <a.rittler@rittler.co>
2026-02-05 11:18:53 +0000

    File changes

--- src/pages/OfferDetail.jsx
+++ src/pages/OfferDetail.jsx
@@ -172,7 +172,7 @@ export default function OfferDetail() {
       if (isNewOffer) {
         // Generate offer number
         const offerCount = await base44.entities.Offer.list().then(offers => offers.length);
-        const offerNumber = `OFF-${String(offerCount + 1).padStart(5, '0')}`;
+        const offerNumber = `2026/${String(offerCount + 1).padStart(6, '0')}`;
         
         const newOffer = await base44.entities.Offer.create({
           ...formData,
