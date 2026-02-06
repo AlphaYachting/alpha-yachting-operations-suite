a023970a94d5a453601b51f5476b6cc440797f88
a.rittler <a.rittler@rittler.co>
2026-02-06 13:36:31 +0000

    File changes

--- src/pages/CustomerPortalTest.jsx
+++ src/pages/CustomerPortalTest.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
+import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
